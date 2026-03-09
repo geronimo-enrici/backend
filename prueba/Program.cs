a3bff41fcb30af0dff3b7e84c33acef4104c6228
Geronimo <geronimo.enrici@koovra.com>
2026-03-09 10:37:57 -0300

    Habilitar Swagger en produccion y ajustar rutas

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -44,11 +44,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{ 
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Mi API Veterinaria V1");
+    c.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 app.UseCors("AllowAll");
