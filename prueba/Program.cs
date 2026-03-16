b1c1b407f11dba8d469d500c7c295bd9ee9c6485
Geronimo <geronimo.enrici@koovra.com>
2026-03-16 10:45:29 -0300

    https

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -50,7 +50,6 @@ app.UseSwaggerUI(c =>
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Mi API Veterinaria V1");
 });
 
-app.UseHttpsRedirection();
 app.UseCors("AllowAll");
 app.UseAuthentication();
 app.UseAuthorization();
